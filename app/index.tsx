d40549fbadd6c299999d1d24178c903ee161ca83
MargaretLai <MargaretLUI01@gmail.com>
2025-05-27 18:04:11 -0400

    Installed UI Library and tested it out
    Installed React Native Paper and added a button to test it out. The frontend should be properly set up now.

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import { Button } from "react-native-paper";
 
 export default function Index() {
   return (
@@ -10,6 +11,9 @@ export default function Index() {
       }}
     >
       <Text>Remember to use React Native Paper guys!</Text>
+      <Button mode="contained" onPress={() => console.log("Pressed!")}>
+        I added this btn to test out the library.
+      </Button>
     </View>
   );
 }
