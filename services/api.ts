0e4debaea15386f394c937110e6bf67e53d943ef
MargaretLai <MargaretLUI01@gmail.com>
2025-06-05 19:22:20 -0400

    Debug: alert doesn't go with this file
    Changed to console.log()

--- services/api.ts
+++ services/api.ts
@@ -1,34 +1,38 @@
 import axios from "axios";
 
 const API_URL = "http://192.168.1.100:8080/api";
-let apiToken : any;
+let apiToken: any;
 
-export function getAPIToken(){
+export function getAPIToken() {
   return apiToken;
 }
-
 
 const api = axios.create({
   baseURL: API_URL,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
-  }
+  },
 });
 
 //Login credentials
 const credentials = {
-    username: "user",
-    password: "password"
-  };
+  username: "user",
+  password: "password",
+};
 
 //Initial connection with API
-api.post(`/login`, credentials, {
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded"
-      }
-    })
-  .then((response) => {apiToken = response.data})
-  .catch((error) => { alert("An error has occurred with API, please try again...")});
+api
+  .post(`/login`, credentials, {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  })
+  .then((response) => {
+    apiToken = response.data;
+  })
+  .catch((error) => {
+    console.error("API login error:", error.response?.data || error.message);
+  });
 
 export default api;
