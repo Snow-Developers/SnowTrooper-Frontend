383df44f7357892327f5b860e34df52d5580d2ec
danielgof <gofmandon@gmail.com>
2025-06-05 22:34:37 -0400

    home page and fetch weather

--- services/api.ts
+++ services/api.ts
@@ -1,34 +1,34 @@
-import axios from "axios";
+// import axios from "axios";
 
-const API_URL = "http://192.168.1.100:8080/api";
-let apiToken : any;
+// const API_URL = "http://192.168.1.100:8080/api";
+// let apiToken : any;
 
-export function getAPIToken(){
-  return apiToken;
-}
+// export function getAPIToken(){
+//   return apiToken;
+// }
 
 
-const api = axios.create({
-  baseURL: API_URL,
-  timeout: 10000,
-  headers: {
-    "Content-Type": "application/json",
-  }
-});
+// const api = axios.create({
+//   baseURL: API_URL,
+//   timeout: 10000,
+//   headers: {
+//     "Content-Type": "application/json",
+//   }
+// });
 
-//Login credentials
-const credentials = {
-    username: "user",
-    password: "password"
-  };
+// //Login credentials
+// const credentials = {
+//     username: "user",
+//     password: "password"
+//   };
 
-//Initial connection with API
-api.post(`/login`, credentials, {
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded"
-      }
-    })
-  .then((response) => {apiToken = response.data})
-  .catch((error) => { alert("An error has occurred with API, please try again...")});
+// //Initial connection with API
+// api.post(`/login`, credentials, {
+//       headers: {
+//         "Content-Type": "application/x-www-form-urlencoded"
+//       }
+//     })
+//   .then((response) => {apiToken = response.data})
+//   .catch((error) => { alert("An error has occurred with API, please try again...")});
 
-export default api;
+// export default api;
