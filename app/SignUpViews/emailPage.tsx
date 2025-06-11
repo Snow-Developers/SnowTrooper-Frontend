c62db4f0d5cb9e1bce32c2e708dc2bd091e7cbf8
MargaretLai <MargaretLUI01@gmail.com>
2025-06-11 09:18:59 -0400

    Align "Enter Information" with form in emailPage.tsx

--- app/SignUpViews/emailPage.tsx
+++ app/SignUpViews/emailPage.tsx
@@ -3,180 +3,205 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 import { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { Button, Text, TextInput } from "react-native-paper";
-import { useSignUpContext } from '../../context/SignUpContext';
+import { useSignUpContext } from "../../context/SignUpContext";
 import auth from "../../services/firebaseConfig";
 
-
 export default function EmailPage() {
-    const { setSignUpData } = useSignUpContext();
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [phoneNumber, setPhoneNumber] = useState('');
-    const [userRole, setUserRole] = useState('Customer'); //Default set to customer
-    
+  const { setSignUpData } = useSignUpContext();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [userRole, setUserRole] = useState("Customer"); //Default set to customer
 
-    const handleEmailSignUp = async() => {
-        
-        //Check if password and confirm password match
-        if(password.length >= 6){
-            if (password !== confirmPassword) {
-                console.error('Passwords do not match');
-                alert('Passwords do not match');
-                return;
-            }
-        }else{ 
-            console.error('Password needs to be at least 6 characters long');
-            alert('Password needs to be at least 6 characters long');
-            return;
-        }
+  const handleEmailSignUp = async () => {
+    //Check if password and confirm password match
+    if (password.length >= 6) {
+      if (password !== confirmPassword) {
+        console.error("Passwords do not match");
+        alert("Passwords do not match");
+        return;
+      }
+    } else {
+      console.error("Password needs to be at least 6 characters long");
+      alert("Password needs to be at least 6 characters long");
+      return;
+    }
 
-        //Create user within Firebase Auth
-        await createUserWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                const userUID = userCredential.user.uid;    // Get the user ID
-                console.log('User signed up successfully, userUID: ', userUID);
+    //Create user within Firebase Auth
+    await createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const userUID = userCredential.user.uid; // Get the user ID
+        console.log("User signed up successfully, userUID: ", userUID);
 
-                //Add info to global context
-                setSignUpData({
-                    firstName,
-                    lastName,
-                    email,
-                    phoneNumber,
-                    userRole,
-                });
-                router.push("/SignUpViews/additionalInfo");
-            }).catch((error) => {
-                console.error('Error signing up:', error);
-                alert('Sign Up Failed: ' + error.message);
-            });
-    };
+        //Add info to global context
+        setSignUpData({
+          firstName,
+          lastName,
+          email,
+          phoneNumber,
+          userRole,
+        });
+        router.push("/SignUpViews/additionalInfo");
+      })
+      .catch((error) => {
+        console.error("Error signing up:", error);
+        alert("Sign Up Failed: " + error.message);
+      });
+  };
 
-    return (
-        <View style = {styles.info}>
-            <Text variant="headlineMedium">Enter Information</Text>
-            
-            <View style = {styles.infoTextBox}>
-                <TextInput
-                    mode = "outlined"
-                    label="First Name"
-                    placeholder="Enter your first name"
-                    value={firstName}
-                    onChangeText={text => setFirstName(text)}
-                />
+  return (
+    <View style={styles.info}>
+      <Text variant="headlineMedium" style={styles.title}>
+        Enter Information
+      </Text>
 
-                <TextInput
-                    mode="outlined"
-                    label="Last Name"
-                    placeholder="Enter your last name"
-                    value={lastName}
-                    onChangeText={text => setLastName(text)}
-                />
+      <View style={styles.infoTextBox}>
+        <TextInput
+          mode="outlined"
+          label="First Name"
+          placeholder="Enter your first name"
+          value={firstName}
+          onChangeText={(text) => setFirstName(text)}
+        />
 
-                <TextInput
-                    mode="outlined"
-                    label="Email"
-                    placeholder="Enter your email"
-                    value={email}
-                    onChangeText={text => setEmail(text)}
-                />
+        <TextInput
+          mode="outlined"
+          label="Last Name"
+          placeholder="Enter your last name"
+          value={lastName}
+          onChangeText={(text) => setLastName(text)}
+        />
 
-                <TextInput
-                    mode="outlined"
-                    label="Phone Number"
-                    placeholder="Enter your phone number"
-                    value={phoneNumber}
-                    onChangeText={text => setPhoneNumber(text)}
-                />
+        <TextInput
+          mode="outlined"
+          label="Email"
+          placeholder="Enter your email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
 
-                <TextInput
-                    mode="outlined"
-                    label="Password"
-                    placeholder="Enter a password"
-                    value={password}
-                    onChangeText={text => setPassword(text)}
-                    secureTextEntry
-                />
+        <TextInput
+          mode="outlined"
+          label="Phone Number"
+          placeholder="Enter your phone number"
+          value={phoneNumber}
+          onChangeText={(text) => setPhoneNumber(text)}
+        />
 
-                <TextInput
-                    mode="outlined"
-                    label="Confirm Password"
-                    placeholder="Re-enter your password"
-                    value={confirmPassword}
-                    onChangeText={text => setConfirmPassword(text)}
-                    secureTextEntry
-                />
-            
+        <TextInput
+          mode="outlined"
+          label="Password"
+          placeholder="Enter a password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          secureTextEntry
+        />
 
-                <Text variant="headlineMedium" style = {styles.rolesText}>Select your role</Text>
+        <TextInput
+          mode="outlined"
+          label="Confirm Password"
+          placeholder="Re-enter your password"
+          value={confirmPassword}
+          onChangeText={(text) => setConfirmPassword(text)}
+          secureTextEntry
+        />
 
-                <View style = {styles.roles}>
-                    <Button
-                        mode="outlined"
-                        onPress={() => setUserRole('Customer')}
-                        style = {[styles.roleButton, userRole === 'Customer' && styles.selectedButton]}
-                        labelStyle = {[styles.buttonLabel, userRole === 'Customer' && styles.selectedButtonLabel]}
-                    > Customer </Button>
+        <Text variant="headlineMedium" style={styles.rolesText}>
+          Select your role
+        </Text>
 
-                    <Button
-                        mode="outlined"
-                        onPress={() => setUserRole('Contractor')}
-                        style={[styles.roleButton, userRole === 'Contractor' && styles.selectedButton]}
-                        labelStyle = {[styles.buttonLabel, userRole === 'Contractor' && styles.selectedButtonLabel]}
-                    > Contractor </Button>
-                </View>
+        <View style={styles.roles}>
+          <Button
+            mode="outlined"
+            onPress={() => setUserRole("Customer")}
+            style={[
+              styles.roleButton,
+              userRole === "Customer" && styles.selectedButton,
+            ]}
+            labelStyle={[
+              styles.buttonLabel,
+              userRole === "Customer" && styles.selectedButtonLabel,
+            ]}
+          >
+            {" "}
+            Customer{" "}
+          </Button>
 
-                <Button
-                    mode="contained"
-                    onPress={handleEmailSignUp}
-                    style= {styles.signupButton}
-                > Sign Up </Button>
-            </View>
+          <Button
+            mode="outlined"
+            onPress={() => setUserRole("Contractor")}
+            style={[
+              styles.roleButton,
+              userRole === "Contractor" && styles.selectedButton,
+            ]}
+            labelStyle={[
+              styles.buttonLabel,
+              userRole === "Contractor" && styles.selectedButtonLabel,
+            ]}
+          >
+            {" "}
+            Contractor{" "}
+          </Button>
         </View>
-    );
+
+        <Button
+          mode="contained"
+          onPress={handleEmailSignUp}
+          style={styles.signupButton}
+        >
+          {" "}
+          Sign Up{" "}
+        </Button>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    info : {
-        flex: 1,
-        justifyContent: "center",
-        width: "100%",
-    },
-    infoTextBox : {
-        paddingHorizontal: 10,
-        flexDirection: "column",
-        gap: 15,
-    },
-    roles : {
-        marginTop: 5,
-        flexDirection: "row",
-        justifyContent: "center",
-        gap: 50,
-        alignItems: "center"
-    },
-    roleButton : {
-        marginVertical: 20,
-        width: 150,
-    },
-    selectedButton : {
-        backgroundColor: '#00bedc',
-        color: '#ffffff'
-    },
-    buttonLabel: {
-        color: '#00bedc',
-        fontSize: 18, 
-        fontWeight: 'bold'
-    },
-    selectedButtonLabel: {
-        color: '#ffffff' 
-    },
-    rolesText : {
-        marginTop: 20,
-        textAlign: "center",
-    },
-    signupButton: {
-        paddingHorizontal: 10,
-    }
+  title: {
+    paddingHorizontal: 10,
+  },
+  info: {
+    flex: 1,
+    justifyContent: "center",
+    width: "100%",
+  },
+  infoTextBox: {
+    paddingHorizontal: 10,
+    flexDirection: "column",
+    gap: 15,
+  },
+  roles: {
+    marginTop: 5,
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: 50,
+    alignItems: "center",
+  },
+  roleButton: {
+    marginVertical: 20,
+    width: 150,
+  },
+  selectedButton: {
+    backgroundColor: "#00bedc",
+    color: "#ffffff",
+  },
+  buttonLabel: {
+    color: "#00bedc",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  selectedButtonLabel: {
+    color: "#ffffff",
+  },
+  rolesText: {
+    marginTop: 20,
+    textAlign: "center",
+  },
+  signupButton: {
+    paddingHorizontal: 10,
+  },
 });
