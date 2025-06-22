e14e11ce31da562226a919f892ff627cb5fc6114
MargaretLai <MargaretLUI01@gmail.com>
2025-06-22 14:09:11 -0400

    Web: map component

---
+++ components/driverMap.web.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function DriverMap() {
+  return (
+    <View
+      style={{ height: 300, justifyContent: "center", alignItems: "center" }}
+    >
+      <Text>Map is not supported on web currently.</Text>
+    </View>
+  );
+}
