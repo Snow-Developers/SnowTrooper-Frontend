4fe0f8327e21581f967944f9c0ecb9ef3a512840
MargaretLai <MargaretLUI01@gmail.com>
2025-07-03 19:24:31 -0400

    Wrap app with stripe provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,15 @@
 import api, { getAPIToken } from "@/services/api";
+import { StripeProvider } from "@stripe/stripe-react-native";
 import { router, Stack, usePathname } from "expo-router";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
-import { Appbar, DefaultTheme, Provider as PaperProvider } from "react-native-paper";
-import { SignUpProvider } from '../context/SignUpContext';
-
-
+import {
+  Appbar,
+  DefaultTheme,
+  Provider as PaperProvider,
+} from "react-native-paper";
+import { SignUpProvider } from "../context/SignUpContext";
 
 const customTheme = {
   ...DefaultTheme,
@@ -24,44 +27,48 @@ export default function RootLayout() {
   const [role, setRole] = useState(" ");
 
   useEffect(() => {
-      const unsubscribe = onAuthStateChanged(getAuth(), (user : any) => {
-        if(user){ 
-          
-          api.get(`/users/${user.uid}`,
-              {
-                headers: {
-                  Authorization: `Bearer ${getAPIToken()}`,
-                  'Content-Type': 'application/json',
-                  "ngrok-skip-browser-warning": "11111",
-                }
-         }).then((result) => {
-            setRole(result.data.role || ' ');
-            if(result.data.uid){
+    const unsubscribe = onAuthStateChanged(getAuth(), (user: any) => {
+      if (user) {
+        api
+          .get(`/users/${user.uid}`, {
+            headers: {
+              Authorization: `Bearer ${getAPIToken()}`,
+              "Content-Type": "application/json",
+              "ngrok-skip-browser-warning": "11111",
+            },
+          })
+          .then((result) => {
+            setRole(result.data.role || " ");
+            if (result.data.uid) {
               console.log("Path: ", pathname);
               console.log("User profile data role: ", result.data.role);
-              if(pathname === "/" && result.data.role === "Customer"){
+              if (pathname === "/" && result.data.role === "Customer") {
                 router.replace("/(tabs)/customerHomeScreen");
-              }else if(pathname === "/" && result.data.role === "Contractor"){
+              } else if (
+                pathname === "/" &&
+                result.data.role === "Contractor"
+              ) {
                 router.replace("/(tabs)/contractorHomeScreen");
               }
               console.log("User is currently logged in: ", user);
-              
             }
-         }).catch((error) => {
+          })
+          .catch((error) => {
             console.log("An error has occurred: ", error);
-            if(error.status === 404){
-              console.log("User profile cannot be found within Firestore or user profile has not been created yet");
+            if (error.status === 404) {
+              console.log(
+                "User profile cannot be found within Firestore or user profile has not been created yet"
+              );
             }
-         });
-          
-        }else{
-          console.log("No user is currently logged in");
-        }
-        setIsAuthenticated(!!user);
-      });
-      return unsubscribe;
+          });
+      } else {
+        console.log("No user is currently logged in");
+      }
+      setIsAuthenticated(!!user);
+    });
+    return unsubscribe;
   }, [pathname]);
-/*
+  /*
   const appBarPath: { [key: string]: string } = {
     "/homeScreen": "Home",
     "/profileScreen": "Profile",
@@ -82,44 +89,45 @@ export default function RootLayout() {
 
   return (
     <PaperProvider theme={customTheme}>
+      <StripeProvider publishableKey="pk_test_51PoUcnHOAzpYfGljjhrhSD3b0Mu9XErUdWlioMR2pGTbVdxv7l5u917QGX2KhOJek6zJMpQ46kFsbmu94F8UPZvV00R14w1MES">
         <SignUpProvider>
           {isAuthenticated && (
-            <Appbar.Header style = {{
-            height: 50,
-            backgroundColor: "#00c1de",
-          }}
-          mode = "center-aligned">
-            <Appbar.Content title= {appBarPath[pathname] || "SnowTroopers"} titleStyle = {styles.appBarTitle}/>
-          </Appbar.Header>
-          )}
-            <Stack
-              screenOptions={{
-                headerShown: false,
-                contentStyle: { backgroundColor: "#fff" },
+            <Appbar.Header
+              style={{
+                height: 50,
+                backgroundColor: "#00c1de",
               }}
-            />
-          
+              mode="center-aligned"
+            >
+              <Appbar.Content
+                title={appBarPath[pathname] || "SnowTroopers"}
+                titleStyle={styles.appBarTitle}
+              />
+            </Appbar.Header>
+          )}
+          <Stack
+            screenOptions={{
+              headerShown: false,
+              contentStyle: { backgroundColor: "#fff" },
+            }}
+          />
         </SignUpProvider>
+      </StripeProvider>
     </PaperProvider>
   );
 }
 
-
 const styles = StyleSheet.create({
-  
-  
   appBarTitle: {
-    fontFamily: "tt-supermolot-neue-trl"
-
+    fontFamily: "tt-supermolot-neue-trl",
   },
   background: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: 442, 
+    justifyContent: "center",
+    alignItems: "center",
+    width: 442,
     height: 275,
     marginBottom: 30,
     opacity: 0.04,
-
   },
-})
+});
